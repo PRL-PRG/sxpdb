3e6bd9e6f89cfce9a14ccfaa3013d4c2d990146d
Pierre Donat-Bouillud <pierre.donat.bouillud@gmail.com>
2021-10-26 15:39:57 +0200

    Adding the global store

---
+++ src/global_store.h
@@ -0,0 +1,42 @@
+#ifndef SXPDB_GLOBAL_STORE_H
+#define SXPDB_GLOBAL_STORE_H
+
+#include "store.h"
+#include "default_store.h"
+
+#include <vector>
+
+class GlobalStore : Store {
+  private:
+    // Stores the names of the various stores, their types, their number of values
+    // it is redundant with the configuration files of the various stores, but it is ok
+    std::string configuration_name;
+    std::vector<DefaultStore> stores;
+
+    size_t bytes_read;
+
+  protected:
+    virtual void load_index();
+    virtual void load_metadata();
+    virtual void write_index();
+
+  public:
+    GlobalStore(const std::string& description_name);
+
+    virtual bool load();
+
+    virtual bool merge_in(const std::string& filename);
+
+    virtual bool add_value(SEXP val);
+    virtual bool have_seen(SEXP val) const;
+
+    virtual SEXP get_value(size_t index) const;
+
+    // Pass it a Description and a Distribution that precises what kind of values
+    // we want
+    virtual SEXP sample_value() const;
+
+    virtual ~GlobalStore();
+};
+
+#endif
