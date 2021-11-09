9654d92d4ebe79dfccf6fd3f9a60f6fcd6c33431
Pierre Donat-Bouillud <pierre.donat.bouillud@gmail.com>
2021-11-09 11:25:47 +0100

    Export metadata as a data frame

--- R/sxpdb.R
+++ R/sxpdb.R
@@ -88,3 +88,7 @@ view_db <- function(db) {
 	viewer
 }
 
+#' @export
+view_meta_db <- function(db) {
+  do.call(rbind.data.frame, lapply(seq_len(size_db(db) - 1), function(i) as.data.frame(.Call(SXPDB_get_meta_idx, db, i))))
+}
