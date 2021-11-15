47672864a5a5a83306d2df9431f2737d68fd2a91
programLyrique <pierre.donat.bouillud@gmail.com>
2021-11-15 08:47:22 -0500

    Ignore environments

--- src/global_store.cpp
+++ src/global_store.cpp
@@ -100,11 +100,16 @@ bool GlobalStore::merge_in(GlobalStore& gstore) {
 }
 
 bool GlobalStore::add_value(SEXP val) {
+  // Ignore environments
+  if(TYPEOF(val) == ENVSXP) {
+    return false;  
+  }
+  
   // we assume there is at least a "any" store
   auto it = types.find(Rf_type2char(TYPEOF(val)));
   size_t store_index= 0;
 
-  if(it != nullptr) {
+  if(it != types.end()) {
     store_index = it->second;
   }
   else {
@@ -126,7 +131,7 @@ bool GlobalStore::have_seen(SEXP val) const {
   auto it = types.find(Rf_type2char(TYPEOF(val)));
   size_t store_index= 0;
 
-  if(it != nullptr) {
+  if(it != types.end()) {
     store_index = it->second;
   }
   else {
@@ -141,7 +146,7 @@ SEXP GlobalStore::get_metadata(SEXP val) const {
   auto it = types.find(Rf_type2char(TYPEOF(val)));
   size_t store_index= 0;
 
-  if(it != nullptr) {
+  if(it != types.end()) {
     store_index = it->second;
   }
   else {
