677645524aabe1acfab1a708fd5d5c3d8df7d03f
programLyrique <pierre.donat.bouillud@gmail.com>
2022-09-15 06:03:14 -0400

    Better test harness

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -10,8 +10,16 @@ db_from_values <- function(values, origins=NULL, name="sxpdb") {
     # It will actually create a directory with this name there
     db <- open_db(tempfile(name), mode=TRUE)
 
-    for(v in values) {
-        add_val_origin(db, v, "pkg", "f", "arg")
+    if(is.null(origins)) {
+        for(v in values) {
+            add_val_origin(db, v, "pkg", "f", "arg")
+        }
+    }
+    else {
+        stopifnot(length(values) == length(origins))
+        for(i in seq_along(values)) {
+            add_val_origin(db, values[[i]], origins[[i]][[1]], origins[[i]][[2]], origins[[i]][[3]])
+        }
     }
     db
 }
--- tests/testthat/test-db.R
+++ tests/testthat/test-db.R
@@ -1,5 +1,5 @@
 test_that("open db in read mode", {
-    db <- db_from_values(c(1L, "tu", 45.9))
+    db <- db_from_values(list(1L, "tu", 45.9))
     path <- path_db(db)
     close(db)
 
@@ -8,8 +8,38 @@ test_that("open db in read mode", {
     close(db)
 })
 
+test_that("add_val_origin", {
+    db <- open_db(tempfile("sxpdb"), mode = TRUE)
+    add_val_origin(db, 34, "pkg", "f", "arg")
+
+    v <- get_value_idx(db, 0)
+    expect_equal(v, 34)
+
+    origs <- get_origins_idx(db, 0)
+    expect_equal(origs$pkg, "pkg")
+    expect_equal(origs$fun, "f")
+    expect_equal(origs$param, "arg")
+
+    close_db(db)
+})
+
+test_that("read values", {
+    l <- list(1L, "tu", 45.9)
+    db <- db_from_values(l)
+    path <- path_db(db)
+    close(db)
+
+    db <- open_db(path)
+    v <- view_db(db)
+    for(i in seq_along(l)) {
+        expect_equal(v[[i]], l[[i]])
+    }
+
+    close(db)
+})
+
 test_that("open_db in write mode", {
-    db <- db_from_values(c(1L, "tu", 45.9))
+    db <- db_from_values(list(1L, "tu", 45.9))
     path <- path_db(db)
     close(db)
 
@@ -18,12 +48,12 @@ test_that("open_db in write mode", {
     close(db)
 })
 
-test_that("open_db in merge mode", {
-    db <- db_from_values(c(1L, "tu", 45.9))
-    path <- path_db(db)
-    close(db)
+# test_that("open_db in merge mode", {
+#     db <- db_from_values(c(1L, "tu", 45.9))
+#     path <- path_db(db)
+#     close(db)
 
-    db <- open_db(path, mode = "merge")
-    expect_equal(class(db), "sxpdb")
-    close(db)
-})
+#     db <- open_db(path, mode = "merge")
+#     expect_equal(class(db), "sxpdb")
+#     close(db)
+# })
