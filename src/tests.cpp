79765ddb60ea0b42b94c446c764c85fdecd312b0
Pierre Donat-Bouillud <pierre.donat.bouillud@gmail.com>
2023-03-16 17:50:09 +0100

    More tests for find_na

--- src/tests.cpp
+++ src/tests.cpp
@@ -10,6 +10,8 @@
 // All test files should include the <testthat.h>
 // header file.
 #include <testthat.h>
+#include <algorithm>
+
 #include "search_index.h"
 
 
@@ -17,9 +19,37 @@ context("Index tests") {
 
   test_that("find_na on sexp_view") {
     Serializer ser(64);
-    
-    const sexp_view_t sexp_view = Serializer::unserialize_view(ser.serialize(Rf_ScalarReal(NA_REAL)));
 
+    // Scalar NA real
+    SEXP scalar_na = PROTECT(Rf_ScalarReal(NA_REAL));
+    sexp_view_t sexp_view = Serializer::unserialize_view(ser.serialize(scalar_na));
+    UNPROTECT(1);
+    expect_true(find_na(sexp_view));
+
+    // Non-scalar vec with one NA
+    SEXP vec_with_na = PROTECT(Rf_allocVector(REALSXP, 10));
+    std::fill_n(REAL(vec_with_na), 9, 0.);
+    REAL(vec_with_na)[9] = NA_REAL;
+    sexp_view =  Serializer::unserialize_view(ser.serialize(vec_with_na));
+    UNPROTECT(1);
+    expect_true(find_na(sexp_view));
+
+    // character vector
+    SEXP na_string = PROTECT(Rf_ScalarString(NA_STRING));
+    sexp_view =  Serializer::unserialize_view(ser.serialize(na_string));
+    UNPROTECT(1);
+    expect_true(find_na(sexp_view));
+
+    // character vectors
+
+    SEXP na_strings = PROTECT(Rf_allocVector(STRSXP, 10));
+    const char* s = "aaaaaaaaaaaaa";
+    for(int i = 0 ; i < 9 ; i++) {
+      SET_STRING_ELT(na_strings, i, Rf_mkCharLen(s, i));
+    }
+    SET_STRING_ELT(na_strings, 9, NA_STRING);
+    sexp_view =  Serializer::unserialize_view(ser.serialize(na_strings));
+    UNPROTECT(1);
     expect_true(find_na(sexp_view));
   }
 
