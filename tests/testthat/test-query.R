b4a53303bf969cef6a2c3f26f24615947613ff20
Pierre Donat-Bouillud <pierre.donat.bouillud@gmail.com>
2023-03-21 17:22:41 +0100

    Adding test for query on class names

--- tests/testthat/test-query.R
+++ tests/testthat/test-query.R
@@ -1,3 +1,4 @@
+
 test_that("basic sampling", {
      l <- list(1L, "tu", 45.9)
      db <- db_from_values(l)
@@ -70,7 +71,7 @@ test_that("relaxed query", {
 
 
 test_that("query from plan", {
-    l <- list(1L, "tu", 45.9, NA_integer_, TRUE, c(2.1, 4), NA_real_)  
+    l <- list(1L, "tu", 45.9, NA_integer_, TRUE, c(2.1, 4), NA_real_)
     db <- db_from_values(l, with_search_index=TRUE)
 
      expect_equal(nb_values_db(db), length(l))
@@ -93,7 +94,30 @@ test_that("query from plan", {
 
 })
 
-# TODO: 
+test_that("class names", {
+     o1 <- structure(3:8, class="some_class")
+     o2 <- structure("hello world", class="another_class")
+
+     l <- list(1L, "tu", 45.9, NA_integer_, o2, TRUE, c(2.1, 4), o1)
+     db <- db_from_values(l, with_search_index=TRUE)
+
+     expect_equal(nb_values_db(db), length(l))
+
+     q <- query_from_plan(list("classname" = "some_class"))
+     res <- view_db(db, q)
+     expect_length(res, 1)
+     expect_equal(res[[1]], o1)
+
+     q <- query_from_plan(list("classname" = TRUE))
+     res <- view_db(db, q)
+     expect_length(res, 2)
+     expect_equal(res[[1]], o2)
+     expect_equal(res[[2]], o1)
+
+     close(db)
+})
+
+# TODO:
 # - test class names
 # - multi-dimentional values
 # - with attributes
